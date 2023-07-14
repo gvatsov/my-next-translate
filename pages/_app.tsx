a5eac5547fd6d1ad84b2c0a70a712ee9b25276bf
clement <74004967+clemgat33@users.noreply.github.com>
2023-07-14 18:11:13 +0300

    add theme back

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,8 @@ import "styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <ThemeProvider>
-    <Component {...pageProps} />
-    // </ThemeProvider>
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
   );
 }
